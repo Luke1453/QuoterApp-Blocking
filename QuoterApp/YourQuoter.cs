4791013ae013bef2e106bf6fa4d6f8ba66273abb
Luke <lukjut@gmail.com>
2024-01-02 16:35:38 +0200

    Add project files.

---
+++ QuoterApp/YourQuoter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuoterApp
+{
+    public class YourQuoter : IQuoter
+    {
+        public double GetQuote(string instrumentId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetVolumeWeightedAveragePrice(string instrumentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
